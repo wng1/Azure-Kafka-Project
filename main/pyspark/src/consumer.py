5cca8dacf36afa450bab1730b3ce7e92138c595f
W N <wng1@users.noreply.github.com>
2025-03-11 16:38:15 +0000

    Update consumer.py

--- main/pyspark/src/consumer.py
+++ main/pyspark/src/consumer.py
@@ -4,3 +4,13 @@ import yaml
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, from_json, avg, count, expr
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType
+
+def load_config_file(config_path: str) -> Dict[str, Any]:
+    """Execute and load the configuration in the YAML file for the application. Eliminate the unnecessary need for hardcoding settings in the code and flexibility for modification and maintenance to the configuration."""
+    try:
+      with open(config_path, 'r') as config_file:
+        return yaml.safe_load(config_file)
+    """Use yaml.safe_load instead of yaml.load to prevent security risk such as arbitrary code from executing in the YAML file"""
+    except Exception as e:
+      raise RuntimeError(f"Failed to load the configuration specified: {str(e)}")
+    """Handle the errors if attempts fail with useful output message. - Error Handling"
