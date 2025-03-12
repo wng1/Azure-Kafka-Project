85a5074baf5da3ee0d524d76eba11a9bc90bb1dd
W N <wng1@users.noreply.github.com>
2025-03-12 19:51:53 +0000

    Update producer.py

--- main/producer/producer.py
+++ main/producer/producer.py
@@ -17,4 +17,34 @@ producer_config = {
 
 producer = Producer(producer_config)
 
+def retrieve_data():
 
+    try:
+
+    except requests.RequestException as e:
+        print(f"Error fetching data: {e}")
+        return None 
+
+def delivery_report(err, msg):
+    if err:
+        print(f"❌ Message delivery failed: {err}")
+    else:
+        print(f"✅ Message delivered to {msg.topic()} [{msg.partition()}]")
+
+def produce_messages():
+  message_data = retrieve_data()
+
+  if message_data:
+    for message in message_data.get("results", []):
+      message_json = json.dumps(message)
+      print(f"{str(message["message"]{message_json}")
+      producer.produce(TOPIC_NAME, key=str______), value=message_json, callback=delivery_report)
+      time.sleep(0.6)
+      producer.flush()
+
+if __name__ == "__main__":
+    produce_messages()
+
+
+
+    
