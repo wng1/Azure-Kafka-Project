29e89fbf7cc573fbc619a64f306a5e333c7035fd
W N <wng1@users.noreply.github.com>
2025-03-12 18:56:51 +0000

    Update producer.py

--- main/producer/producer.py
+++ main/producer/producer.py
@@ -1,6 +1,20 @@
 from confluent_kafka import Producer
-
+"""Utilisation of confluent_kafka on the wrapped around C library, much faster and more efficient than kafka-python"""
 import json
 import requests
 import time
+
+#Kafka fixed variables 
+KAFKA_BROKER = "localhost:9092"
+TOPIC_NAME = "orders"
+
+#Kafka configuration
+producer_config = {
+  "bootstrap.servers": KAFKA_BROKER, 
+  "acks": "all",
+  "retries": 3
+}
+
+producer = Producer(producer_config)
+
 
